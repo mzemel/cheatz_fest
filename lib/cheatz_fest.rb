6058a25e36f1df1f4247871316e22d19c2c6a5c1
Michael.Zemel <michael.zemel@phishme.com>
2016-08-29 11:53:04 -0400

    Change module to include in class

--- lib/cheatz_fest.rb
+++ lib/cheatz_fest.rb
@@ -1,42 +1,50 @@
 require "cheatz_fest/version"
+require 'pry'
 
 module CheatzFest
-  class << self
-    def cheat
-      method_def, *spec = spec_as_string(get_test_line_range)
-      assert = spec.pop
-      spec = spec.map {|l| l += "\n"; l}
-      eval spec.join
-    end
 
-    private
-
-    def spec_file
-      @spec_file ||= set_spec_file
-    end
-
-    def set_spec_file
-      spec_file = {}
-      File.read("bottles/test/bottles_test.rb").each_line.with_index do |line, i|
-        spec_file[i+1] = line.chomp
+  def self.included(base)
+    base.class_eval do
+      def cheat(*args)
+        method_def, *spec = spec_as_string(get_test_line_range)
+        assert = spec.pop
+        spec = spec.map {|l| l += "\n"; l}
+        eval spec.join
       end
-      spec_file
-    end
 
-    def spec_as_string(range)
-      range.map {|l| @spec_file[l] }
-    end
+      private
 
-    def get_test_line_range
-      assert_line, method_def = get_line_and_method
-      method_def_line, _ = spec_file.find {|k, v| v =~ /#{method_def}/}
-      method_def_line .. assert_line.to_i
-    end
+      def spec_file
+        @spec_file ||= set_spec_file
+      end
 
-    def get_line_and_method
-      caller.map do |l|
-        l.match(/^bottles\/test\/bottles_test.rb:(\d+):in `([a-z\d_]+)'/).captures rescue nil
-      end.compact.flatten
+      def set_spec_file
+        spec_file = {}
+        File.read("#{klass}/test/#{klass}_test.rb").each_line.with_index do |line, i|
+          spec_file[i+1] = line.chomp
+        end
+        spec_file
+      end
+
+      def spec_as_string(range)
+        range.map {|l| @spec_file[l] }
+      end
+
+      def get_test_line_range
+        assert_line, method_def = get_line_and_method
+        method_def_line, _ = spec_file.find {|k, v| v =~ /#{method_def}/}
+        method_def_line .. assert_line.to_i
+      end
+
+      def get_line_and_method
+        caller.map do |l|
+          l.match(/^#{klass}\/test\/#{klass}_test.rb:(\d+):in `([a-z\d_]+)'/).captures rescue nil
+        end.compact.flatten
+      end
+
+      def klass
+        self.class.to_s.downcase
+      end
     end
   end
 end
